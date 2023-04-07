957214d3e1b47344ced3e43a5fba36a9608220bc
Yut8853 <hello@junkbranding.com>
2023-04-07 12:42:39 +0900

    見た目と数字表示まで

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ window.addEventListener('load', () => {
   document.body.appendChild(parentdiv);
   // 親のラッパースタイル
   parentdiv.style.width = '100%';
-  parentdiv.style.height = '100vh';
+  parentdiv.style.height = '80vh';
   parentdiv.style.display = 'flex';
-  parentdiv.style.flexWrap = 'wrap';
   parentdiv.style.justifyContent = 'center';
   parentdiv.style.alignItems = 'center';
-  parentdiv.style.gap = '2%';
+  parentdiv.style.flexDirection = 'column';
+  parentdiv.style.gap = '30px';
 
   // ボタン生成用の配列
   const arry = [
@@ -20,46 +20,49 @@ window.addEventListener('load', () => {
   ];
 
   for (let i = 0; i < arry.length; i++) {
+    const rowdiv = document.createElement('div');
+    rowdiv.style.display = 'flex';
+    rowdiv.style.flexDirection = 'row';
+    rowdiv.style.gap = '30px';
+
     for (let j = 0; j < arry[i].length; j++) {
       const calcBtn = document.createElement('div');
       // 子のスタイル
-      calcBtn.style.width = '22%';
-      calcBtn.style.height = '10%';
       calcBtn.style.display = 'flex';
       calcBtn.style.justifyContent = 'center';
       calcBtn.style.alignItems = 'center';
+      calcBtn.style.width = '200px';
+      calcBtn.style.height = '100px';
       calcBtn.style.background = '#000';
       calcBtn.style.color = '#fff';
       calcBtn.style.borderRadius = '15px';
 
       calcBtn.textContent = arry[i][j];
-      parentdiv.appendChild(calcBtn);
+      rowdiv.appendChild(calcBtn);
 
       calcBtn.addEventListener('click', () => {
-        calcBtnValue = calcBtn.innerHTML;
-        result.value += calcBtnValue;
-
-        if (result.value) {
-          result.value
-        } else {
-          console.log('no')
-        }
+        const calcBtnValue = arry[i][j];
+        result.textContent += calcBtnValue;
       });
     }
+    parentdiv.appendChild(rowdiv);
   }
 
   // 計算結果とACボタン横並び
   const resultWrapper = document.createElement('div');
   resultWrapper.style.display = 'flex';
   resultWrapper.style.justifyContent = 'center';
-  resultWrapper.style.alignItems = 'bottom';
-  parentdiv.appendChild(resultWrapper)
+  resultWrapper.style.alignItems = 'center';
+  document.body.appendChild(resultWrapper)
 
   // 計算結果表示
-  let result = document.createElement('input')
-  result.style.width = '100%'
-  result.style.border = 0
-  result.style.borderBottom = 'solid'
+  const result = document.createElement('div')
+  result.style.width = '100%';
+  result.style.height = '80px';
+  result.style.border = '1px solid black';
+  result.style.textAlign = 'right';
+  result.style.fontSize = '30px';
+  result.style.padding = '10px';
   resultWrapper.appendChild(result)
 
   // ACボタン表示
@@ -68,5 +71,4 @@ window.addEventListener('load', () => {
   clearBtn.style.height = '50px'
   clearBtn.innerHTML = 'AC'
   resultWrapper.appendChild(clearBtn)
-
 });
