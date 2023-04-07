748f84c39fb2a4523c887f718fc80bdda5e0f3bb
Yut8853 <hello@junkbranding.com>
2023-04-08 08:56:44 +0900

    スタイル関数化

--- app.js
+++ app.js
@@ -2,17 +2,11 @@ window.addEventListener('load', () => {
 
   const parentdiv = document.createElement('div');
   document.body.appendChild(parentdiv);
-  // 親のラッパースタイル
-  // parentdiv.style.width = '100%';
-  // parentdiv.style.height = '80vh';
-  // parentdiv.style.display = 'flex';
-  // parentdiv.style.justifyContent = 'center';
-  // parentdiv.style.alignItems = 'center';
-  // parentdiv.style.flexDirection = 'column';
-  // parentdiv.style.gap = '30px';
-
-  function setStyle(elm, style) {
-    Object.assign(elm.style, style);
+  
+  function setStyle(elm, styles) {
+    for (let key in styles) {
+      elm.style[key] = styles[key];
+    }
   }
 
   setStyle(parentdiv, {
@@ -35,21 +29,27 @@ window.addEventListener('load', () => {
 
   for (let i = 0; i < arry.length; i++) {
     const rowdiv = document.createElement('div');
-    rowdiv.style.display = 'flex';
-    rowdiv.style.flexDirection = 'row';
-    rowdiv.style.gap = '30px';
+
+    setStyle(rowdiv, {
+      display: 'flex',
+      flexDirection: 'row',
+      gap: '30px',
+    });
 
     for (let j = 0; j < arry[i].length; j++) {
+
       const calcBtn = document.createElement('div');
       // 子のスタイル
-      calcBtn.style.display = 'flex';
-      calcBtn.style.justifyContent = 'center';
-      calcBtn.style.alignItems = 'center';
-      calcBtn.style.width = '200px';
-      calcBtn.style.height = '100px';
-      calcBtn.style.background = '#000';
-      calcBtn.style.color = '#fff';
-      calcBtn.style.borderRadius = '15px';
+      setStyle(calcBtn, {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '200px',
+        height: '100px',
+        background: '#000',
+        color: '#fff',
+        borderRadius: '15px',
+      });
 
       calcBtn.textContent = arry[i][j];
       rowdiv.appendChild(calcBtn);
@@ -64,25 +64,31 @@ window.addEventListener('load', () => {
 
   // 計算結果とACボタン横並び
   const resultWrapper = document.createElement('div');
-  resultWrapper.style.display = 'flex';
-  resultWrapper.style.justifyContent = 'center';
-  resultWrapper.style.alignItems = 'center';
+  setStyle(resultWrapper, {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  });
   document.body.appendChild(resultWrapper)
 
   // 計算結果表示
   const result = document.createElement('div')
-  result.style.width = '100%';
-  result.style.height = '80px';
-  result.style.border = '1px solid black';
-  result.style.textAlign = 'right';
-  result.style.fontSize = '30px';
-  result.style.padding = '10px';
+  setStyle(result, {
+    width: '100%',
+    height: '80px',
+    border: '1px solid black',
+    textAlign: 'right',
+    fontSize: '30px',
+    padding: '10px',
+  });
   resultWrapper.appendChild(result)
 
   // ACボタン表示
   const clearBtn = document.createElement('button')
-  clearBtn.style.width = '50px'
-  clearBtn.style.height = '50px'
-  clearBtn.innerHTML = 'AC'
+  setStyle(clearBtn, {
+    width: '50px',
+    height: '50px',
+  });
+  clearBtn.textContent = 'AC'
   resultWrapper.appendChild(clearBtn)
 });
